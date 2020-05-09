32a31a062e2d7b755ebbced8457fc1bc4b48eeec
Tiago Deliberali <tiago.deliberali@gmail.com>
2020-05-09 00:16:57 -0300

    not working version

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static CONTENT: &[u8] = b"HELLO ALICE";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0x8000 as *mut u8;
+
+    for (i, &byte) in CONTENT.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
